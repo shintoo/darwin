c5b793bcfc51539b47300426230b151464c4e258
shintoo <seanprapp@gmail.com>
2021-10-28 08:56:35 -0400

    Rm rellax dep

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import TitleCard from '../components/home/TitleCard'
-import ImageShow from '../components/home/ImageShow'
 import IntroductionCard from '../components/home/IntroductionCard'
 import CallToActionCard from '../components/home/CallToActionCard'
 import Footer from '../components/home/Footer'
@@ -13,7 +12,6 @@ export default function Home() {
     </Head>
     <TitleCard />
     <IntroductionCard />
-    {/*    <ImageShow /> */}
     <CallToActionCard />
     <Footer />
   </>
