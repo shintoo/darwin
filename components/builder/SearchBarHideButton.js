d8e6a16aa7da66f4f7dbe6a2142cc5afc99f627a
Sean Rapp <sean.p.rapp@nasa.gov>
2021-12-02 23:13:19 -0500

    Restyle search bar hide button

--- components/builder/SearchBarHideButton.js
+++ components/builder/SearchBarHideButton.js
@@ -3,9 +3,10 @@ import styles from "./SearchBarHideButton.module.css"
 export default function SearchBarHideButton({ setter, state }) {
   return (
     <div className={styles.button}
+      style={{transform: state ? "rotate(180deg)" : "unset"}}
       onClick={_ => setter(!state)}
     >
-      {state ? "Show Search" : "Hide Search"}
+      v
     </div>
   )
 }
