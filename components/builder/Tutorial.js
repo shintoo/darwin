c948953b466b93ad4eeab6cdae8aa2544e7fc3df
shintoo <seanprapp@gmail.com>
2022-05-07 12:50:45 -0400

    Tutorial hint, dont show tut by default

--- components/builder/Tutorial.js
+++ components/builder/Tutorial.js
@@ -19,42 +19,84 @@ const frames = [
 ]
 
 export default function Tutorial(props) {
-  const [ stepNumber, setStepNumber ] = useState(0);
-  const [ opened, setOpened ] = useState(true)
+  const [ stepNumber, setStepNumber ] = useState(0)
+  const [ opened, setOpened ] = useState(false)
+  const [ hint, setHint ] = useState(true)
 
   const steps = frames.map(({image, content}) => (
     <>
-      <img src={image} className={styles.image}/>
+      <div className={styles.imagecontainer}>
+        <img src={image} className={styles.image}/>
+      </div>
       <div className={styles.text}>{content}</div>
     </>
   ))
 
-  const handleNext = _ => {
-    if (stepNumber == frames.length - 1)
+  const handlePageChange = op => {
+    if (op == "next" && stepNumber == frames.length - 1) {
       setOpened(false);
+      setStepNumber(0);
+    }
     else
-      setStepNumber(i => i + 1)
+      setStepNumber(i => op == "next" ? i + 1 : i - 1)
   }
 
-  if (!opened)
-    return ""
+  //setTimeout(_ => setHint(false), 15000); 
+
+  const close = _ => {
+    setOpened(false);
+    setStepNumber(0);
+  }
 
   return (
-    <Modal close={_=>setOpened(false)} style={{display: "flex", justifyContent: "right", flexDirection: "column"}}>
-      <div className={styles.counter}>{(stepNumber + 1) + "/" + frames.length}</div>
-      {steps[stepNumber]}
-      <div className={styles.controls}>
-        {stepNumber !== frames.length - 1 && <span className={styles.skip} onClick={_=>setOpened(false)}>skip</span>}
-        <Button onClick={handleNext} style={{
-          fontSize: "1em",
-          boxShadow: "unset",
-          width: "100px",
-          marginTop: "1em"
-        }}>
-          {stepNumber == frames.length - 1 ? "Done" : "Next"}
-        </Button>
+    <>
+    { hint ?
+      <div className={styles.hint} onClick={_ => {setHint(false); setOpened(true)}}>
+        First time? Click here to start the tutorial. 
+        <div
+          style={{position: "absolute", top: "5px", right: "12px", cursor: "pointer", color: "#ccc"}}
+          onClick={e => {e.stopPropagation();  setHint(false)}}>
+          &ndash;
+        </div>
       </div>
-    </Modal>
+     :
+      <div className={styles.showhide} onClick={_ => setOpened(!opened)}>?</div>
+    }
+    { opened && 
+    <Modal close={close} style={{display: "flex", justifyContent: "right", flexDirection: "column", minWidth: "500px"}}>
+        <div className={styles.counter}>{(stepNumber + 1) + "/" + frames.length}</div>
+        {steps[stepNumber]}
+        <div className={styles.controls}>
+          {stepNumber !== frames.length - 1 && <span className={styles.skip} onClick={close}>skip</span>}
+
+          { stepNumber != 0 &&
+          <Button onClick={_ => handlePageChange("prev")} style={{
+            fontSize: "1em",
+            boxShadow: "unset",
+            width: "100px",
+            marginTop: "1em",
+            background: "white",
+            border: "1px solid #8CAB73",
+            color: "#8CAB73",
+            marginLeft: "auto",
+            marginRight: "1rem",
+
+          }}>
+            Previous
+          </Button>
+          }
+          <Button onClick={_ => handlePageChange("next")} style={{
+            fontSize: "1em",
+            boxShadow: "unset",
+            width: "100px",
+            marginTop: "1em"
+          }}>
+            {stepNumber == frames.length - 1 ? "Done" : "Next"}
+          </Button>
+        </div>
+      </Modal>
+    }
+    </>
   )
  
 }
